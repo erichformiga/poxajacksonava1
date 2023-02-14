81dd7e1fc9992f8fec612a10604be13e78f61bc8
erichformiga <erichformiga@gmail.com>
2023-02-13 21:13:44 -0300

    Removendo a Função Sort

--- Program.cs
+++ Program.cs
@@ -8,52 +8,39 @@ namespace ArquivoDesordenado
     {
         static void Main(string[] args)
         {
+           int count = 0;
             // Lê os números do arquivo que estão desordenados
             string[] numbers = File.ReadAllLines("input.txt");
             int[] intNumbers = Array.ConvertAll(numbers, int.Parse);
-            
+            Console.WriteLine($"Valores desordenados: {string.Join(",", intNumbers)}");
             // Usando a função sort consigo ordenar os números
-            Array.Sort(intNumbers);
+            //Array.Sort(intNumbers);
             
-            // Efetuo a gravação dos números ordenados no arquivo
-            using (StreamWriter writer = new StreamWriter("output.txt"))
+            for (int j = 0; j <= numbers.Length - 2; j++)
             {
-                foreach (int number in intNumbers)
+            
+                for (int i = 0; i <= numbers.Length - 2; i++)
                 {
-                    writer.WriteLine(number);
+                    count = count + 1;
+                    if (intNumbers[i] > intNumbers[i + 1])
+                    {
+                        int temp = intNumbers[i + 1];
+                        intNumbers[i + 1] = intNumbers[i];
+                        intNumbers[i] = temp;
+                    }
                 }
+            }
+        
+            Console.WriteLine($"Valores ordenados: {string.Join(",", intNumbers)}");
+
+            // Efetuo a gravação dos números ordenados no arquivo
+            StreamWriter writer = new StreamWriter("output.txt");
+            foreach (int number in intNumbers)
+            {
+                writer.WriteLine(number);
             }
 
             Console.WriteLine("Arquivo gravado com sucesso!");
-            
-            // Caminho utilizado para ler os arquivos com numeros ordenados e desordenados
-            string path = @"C:\Erich\Acadêmico\Senac\Aulas\3-Fase\Desenvolvimento para Desktop\projetos\entrega_13-02-2023\entrega_13-02-2023\input.txt";
-            string path2 = @"C:\Erich\Acadêmico\Senac\Aulas\3-Fase\Desenvolvimento para Desktop\projetos\entrega_13-02-2023\entrega_13-02-2023\output.txt";
-
-            try
-            {   // Ler caminho do arquivo desordenado
-                string text = File.ReadAllText(path);
-                Console.WriteLine("Conteúdo do arquivo desordenado: ");
-                // Imprme em tela o arquivo desordenado
-                Console.WriteLine(text);
-                // Ler caminho do arquivo ordenado
-                string text2 = File.ReadAllText(path2);
-                Console.WriteLine("Conteúdo do arquivo ordenado: ");
-                // Imprime em tela o arquivo ordenado
-                Console.WriteLine(text2);
-            }
-            // Tratamento de mensagens de erro
-            catch (FileNotFoundException)
-            {
-                Console.WriteLine("O arquivo não foi encontrado.");
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Ocorreu um erro ao ler o arquivo.");
-            }
-            // Julguei necessário utilizar o "Console.ReadKey", para que nao seja fechada a tela do terminal. Vi que seria uma boa prática
-            //Console.ReadKey();
-
         }
     }
 }
